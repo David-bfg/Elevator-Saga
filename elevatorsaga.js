7f873a1fcccb53e04c373546d9ef21aab2d41e08
David Garvey <david.1.bfg@gmail.com>
2015-05-12 21:22:34 -0700

    moved var statements into for loops

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,9 +1,9 @@
 {
     init: function(elevators, floors) {
-        var i = 0;
+
         var floorQueue = [];
         var activeFloorQueue = [];
-        for(; i < elevators.length; i++){
+        for(var i = 0; i < elevators.length; i++){
           elevators[i].on("idle", function() {
               if (floorQueue.length!=0){
                   var floorNum = floorQueue.shift();
@@ -13,10 +13,9 @@
                   this.goToFloor(this.currentFloor());
               }
           });
-        
+
           elevators[i].on("floor_button_pressed", function(floorNum) {
-              var index = 0;
-              for(; this.destinationQueue.length > index; index++){
+              for(var index = 0; this.destinationQueue.length > index; index++){
                   if(this.destinationQueue[index] == floorNum){
                       return;
                   }
@@ -25,7 +24,7 @@
                   this.goToFloor(floorNum);
               }
           });
-            
+
           elevators[i].on("passing_floor", function(floorNum, direction) {
               if(this.destinationQueue[0] == floorNum){
                   return;
@@ -35,7 +34,7 @@
                   this.destinationQueue[index] = this.destinationQueue[index - 1];
                   this.goToFloor(floorNum,true);
               }
-              
+
               var users = 0;
               for(var slot in this.userSlots){
                   if(this.userSlots[slot] != null){
@@ -45,7 +44,7 @@
               if(this.maxPassengerCount() == users) {
                   return;
               }
-              
+
               if(-1 != activeFloorQueue.indexOf(floorNum)) {
                   return;
               }
@@ -66,9 +65,9 @@
                       }
                   }
               }
-          
+
           });
-            
+
           elevators[i].on("stopped_at_floor", function(floorNum) {
               if(-1 != floorQueue.indexOf(floorNum)){
                   floorQueue.splice(floorQueue.indexOf(floorNum), 1);
@@ -85,9 +84,9 @@
               }
           });
         }
-        
-        var j = 0;
-        for(; j < floors.length; j++){
+
+
+        for(var j = 0; j < floors.length; j++){
           floors[j].on("up_button_pressed", function() {
               var floorNum = this.floorNum();
               if(-1 == floorQueue.indexOf(floorNum)){
